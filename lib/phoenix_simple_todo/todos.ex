45a3c00d70bbb2b307e138a8d184bb8a631352fb
Evgeny Morozov <evmorov@gmail.com>
2020-07-15 00:46:38 +0300

    Remove doc comments from todos context

--- lib/phoenix_simple_todo/todos.ex
+++ lib/phoenix_simple_todo/todos.ex
@@ -1,199 +1,56 @@
 defmodule PhoenixSimpleTodo.Todos do
-  @moduledoc """
-  The Todos context.
-  """
-
   import Ecto.Query, warn: false
   alias PhoenixSimpleTodo.Repo
+  alias PhoenixSimpleTodo.Todos.{Category, TodoItem}
 
-  alias PhoenixSimpleTodo.Todos.Category
-
-  @doc """
-  Returns the list of categories.
-
-  ## Examples
-
-      iex> list_categories()
-      [%Category{}, ...]
-
-  """
   def list_categories do
     Repo.all(Category)
   end
 
-  @doc """
-  Gets a single category.
-
-  Raises `Ecto.NoResultsError` if the Category does not exist.
-
-  ## Examples
-
-      iex> get_category!(123)
-      %Category{}
-
-      iex> get_category!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_category!(id), do: Repo.get!(Category, id)
 
-  @doc """
-  Creates a category.
-
-  ## Examples
-
-      iex> create_category(%{field: value})
-      {:ok, %Category{}}
-
-      iex> create_category(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_category(attrs \\ %{}) do
     %Category{}
     |> Category.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a category.
-
-  ## Examples
-
-      iex> update_category(category, %{field: new_value})
-      {:ok, %Category{}}
-
-      iex> update_category(category, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_category(%Category{} = category, attrs) do
     category
     |> Category.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a category.
-
-  ## Examples
-
-      iex> delete_category(category)
-      {:ok, %Category{}}
-
-      iex> delete_category(category)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_category(%Category{} = category) do
     Repo.delete(category)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking category changes.
-
-  ## Examples
-
-      iex> change_category(category)
-      %Ecto.Changeset{data: %Category{}}
-
-  """
   def change_category(%Category{} = category, attrs \\ %{}) do
     Category.changeset(category, attrs)
   end
 
-  alias PhoenixSimpleTodo.Todos.TodoItem
-
-  @doc """
-  Returns the list of todo_items.
-
-  ## Examples
-
-      iex> list_todo_items()
-      [%TodoItem{}, ...]
-
-  """
   def list_todo_items do
     Repo.all(TodoItem)
   end
 
-  @doc """
-  Gets a single todo_item.
-
-  Raises `Ecto.NoResultsError` if the Todo item does not exist.
-
-  ## Examples
-
-      iex> get_todo_item!(123)
-      %TodoItem{}
-
-      iex> get_todo_item!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_todo_item!(id), do: Repo.get!(TodoItem, id)
 
-  @doc """
-  Creates a todo_item.
-
-  ## Examples
-
-      iex> create_todo_item(%{field: value})
-      {:ok, %TodoItem{}}
-
-      iex> create_todo_item(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_todo_item(attrs \\ %{}) do
     %TodoItem{}
     |> TodoItem.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a todo_item.
-
-  ## Examples
-
-      iex> update_todo_item(todo_item, %{field: new_value})
-      {:ok, %TodoItem{}}
-
-      iex> update_todo_item(todo_item, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_todo_item(%TodoItem{} = todo_item, attrs) do
     todo_item
     |> TodoItem.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a todo_item.
-
-  ## Examples
-
-      iex> delete_todo_item(todo_item)
-      {:ok, %TodoItem{}}
-
-      iex> delete_todo_item(todo_item)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_todo_item(%TodoItem{} = todo_item) do
     Repo.delete(todo_item)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking todo_item changes.
-
-  ## Examples
-
-      iex> change_todo_item(todo_item)
-      %Ecto.Changeset{data: %TodoItem{}}
-
-  """
   def change_todo_item(%TodoItem{} = todo_item, attrs \\ %{}) do
     TodoItem.changeset(todo_item, attrs)
   end
